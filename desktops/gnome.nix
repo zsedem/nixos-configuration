7958ff45ac8e31e3d3abe8060073403924c28fa5
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2016-12-17 18:27:15 +0100

    add gsettings overrides and refactor gnome specifics to other file
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

---
+++ desktops/gnome.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.xserver.desktopManager.gnome3.extraGSettingsOverrides = ''
+          [org.gnome.desktop.input-sources]
+          sources=[('xkb', 'us'), ('xkb', 'hu')]
+
+          [org.gnome.desktop.peripherals.touchpad]
+          natural-scroll=false
+          tap-to-click=true
+         '';
+  services.gnome3 = {
+      gnome-keyring.enable = true;
+      gnome-documents.enable = false;
+      gnome-user-share.enable = false;
+      gnome-online-accounts.enable = false;
+      seahorse.enable = false;
+      tracker.enable = false;
+    };
+  services.telepathy.enable = false;
+  services.geoclue2.enable = false;
+  services.packagekit.enable = false;
+}
