2dc9aea03502cc26fd18a8ad275fab43b1f5901d
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2017-04-03 22:53:33 +0200

    use kde instead of gnome
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

---
+++ desktops/kde.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  security.pam.services.zsedem.enableKwallet = true;
+  services.xserver = {
+      enable = true;
+      displayManager.sddm = {
+          enable = true;
+          extraConfig = ''
+                        [Autologin]
+                        User=zsedem
+                        Session=plasma.desktop
+                        '';
+      };
+      desktopManager.plasma5 = {
+          enable = true;
+      };
+    };
+  environment.systemPackages  = [ pkgs.ksshaskpass ];
+  networking.networkmanager.enable = true;
+}
