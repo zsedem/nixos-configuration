0b48beee736fe6928d0bf4097b7698809498f756
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2017-04-05 17:01:01 +0200

    enable pulseaudio, it seems kde sound does not work without it
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -16,6 +16,7 @@
           enable = true;
       };
     };
+  hardware.pulseaudio.enable = true;
   environment.systemPackages  = [ pkgs.ksshaskpass ];
   networking.networkmanager.enable = true;
 }
