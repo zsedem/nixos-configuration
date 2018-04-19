d18b60015c1be7c081bccb0873297e17357800eb
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2018-04-19 17:28:45 +0200

    add zsh from config, so completions will be set up
    Signed-off-by: Adam Oliver Zsigmond <azsigmond@hiya.com>

--- commons/command-line-utils.nix
+++ commons/command-line-utils.nix
@@ -6,11 +6,12 @@
         vim = import ../packages/vim/default.nix pkgs;
       in
         (with pkgs; [
-          xclip zsh fzf tmux tldr
+          xclip fzf tmux tldr
           gitAndTools.gitFull gitAndTools.diff-so-fancy tig
           zip unzip openssl
           vim
           htop pstree tree telnet dialog
         ]);
   };
+  programs.zsh.enable = true;
 }
