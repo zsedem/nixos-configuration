d427dd66977867dbb12d40d5dbe720e4f5ce171e
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2018-04-19 11:07:20 +0200

    update to a more module based approach
    Signed-off-by: Adam Oliver Zsigmond <azsigmond@hiya.com>

--- configuration.nix
+++ configuration.nix
@@ -4,65 +4,20 @@
   imports =
     [
       ./hardware-configuration.nix
-      ./desktops/kde.nix
+      ./desktops/gnome.nix
+      ./commons/snapper.nix
+      ./commons/nixos.nix
+      ./commons/systemdboot.nix
+      ./commons/command-line-utils.nix
+      ./commons/docker.nix
+      ./users/azsigmond.nix
       <nixpkgs/nixos/modules/programs/command-not-found/command-not-found.nix>
     ];
-  boot = {
-    cleanTmpDir = true;
-    loader = {
-      efi.canTouchEfiVariables = true;
-      systemd-boot.enable = true;
-    };
-    initrd.luks.devices = [
-      { name = "LinuxDrive"; device = "/dev/sda2"; preLVM = true; }
-    ];
-  };
 
-
-  environment = {
-    systemPackages = let
-        vim = import ./packages/vim/default.nix pkgs;
-        terminal = import ./packages/terminal.nix pkgs;
-      in
-        (with pkgs; [
-          google-chrome
-          xclip zsh fzf tmux tldr
-          (git.override {
-             guiSupport = true;
-             svnSupport = false;
-             sendEmailSupport = false;
-             pythonSupport = false;
-             withManual = false;
-          }) tig git-review
-          zip unzip openssl
-          oraclejdk8
-          vim
-          htop pstree tree telnet dialog
-          terminal
-        ]);
-  };
-
-  nix.gc = {
-    automatic = true;
-    options = "--delete-older-than 7d";
-    dates = "Mon 12:00:00";
-  };
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-
-  security.sudo = {
-    enable = true;
-  };
-
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "Europe/Budapest";
+  environment.systemPackages = let
+      stWithTmux = import ./packages/terminal.nix pkgs;
+    in
+      [ stWithTmux ];
 
   fonts = {
     enableFontDir = true;
@@ -73,32 +28,6 @@
       (import ./packages/monofur.nix)
     ];
   };
-  services = {
-    openssh.enable = true;
-    printing.enable = true;
-    udisks2.enable = true;
-  };
-  programs.bash.enableCompletion = true;
-  users.extraUsers.zsedem = {
-    isNormalUser = true;
-    description = "Zsigmond Ádám Olivér";
-    extraGroups = [ "wheel" "networkmanager" "docker" "vboxusers" ];
-    initialPassword = "titkos";
-    uid = 1000;
-  };
 
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system = {
-    stateVersion = "17.03";
-    autoUpgrade = {
-      enable = true;
-      dates = "12:00";
-    };
-  };
-
-  virtualisation.docker = {
-    enable = true;
-    enableOnBoot = false;
-    liveRestore = false;
-  };
+  networking.networkmanager.useDnsmasq = true;
 }
---
+++ users/azsigmond.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+{
+  users.extraUsers.azsigmond = {
+    isNormalUser = true;
+    description = "Zsigmond Ádám Olivér";
+    extraGroups = [ "wheel" "networkmanager" "docker" "vboxusers" ];
+    initialPassword = "titkos";
+    uid = 1000;
+  };
+}
