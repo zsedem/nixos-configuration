4970324b88fbf0d4c4bbe02d553e71cbbe0deec9
Adam Oliver Zsigmond <azsigmond@hiya.com>
2020-03-16 11:31:18 +0100

    add some changes

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,6 @@
       ./commons/systemdboot.nix
       ./commons/command-line-utils.nix
       ./commons/docker.nix
-      ./commons/powertop.nix
       ./commons/print.nix
       ./local.nix
       ./t470.nix
