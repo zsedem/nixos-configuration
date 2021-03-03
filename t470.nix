c48ff89f14c2848c7a26b15dc31fd0574f514778
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2021-03-03 09:48:08 +0100

    add tlp battery settings for longer battery life

--- t470.nix
+++ t470.nix
@@ -51,4 +51,13 @@ in {
   systemd.services."lenovo_fix".wantedBy = [ "multi-user.target" ];
 
   environment.etc."lenovo_fix.conf".source = "${throttled}/etc/lenovo_fix.conf";
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 70;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT1 = 70;
+      STOP_CHARGE_THRESH_BAT1 = 80;
+    };
+  };
 }
