2d109f5a402e14ffb258812402d68ececae46bfd
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2021-12-19 22:47:58 +0100

    fix preview.sh in git-rg to show the match in the file

--- commons/assets/preview.sh
+++ commons/assets/preview.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #Source: https://github.com/junegunn/fzf.vim/blob/0452b71830b1a219b8cdc68141ee58ec288ea711/bin/preview.sh
+# For local changes just follow the git history
 #MIT License
 #
 #Copyright (c) 2021 Junegunn Choi
@@ -27,14 +28,8 @@ REVERSE="\x1b[7m"
 RESET="\x1b[m"
 
 if [ -z "$1" ]; then
-  echo "usage: $0 [--tag] FILENAME[:LINENO][:IGNORED]"
+  echo "usage: $0 FILENAME[:LINENO][:IGNORED]"
   exit 1
-fi
-
-if [ "$1" = --tag ]; then
-  shift
-  "$(dirname "${BASH_SOURCE[0]}")/tagpreview.sh" "$@"
-  exit $?
 fi
 
 IFS=':' read -r -a INPUT <<< "$1"
@@ -51,20 +46,6 @@ if [[ -n "$CENTER" && ! "$CENTER" =~ ^[0-9] ]]; then
 fi
 CENTER=${CENTER/[^0-9]*/}
 
-# MS Win support
-if [[ "$FILE" =~ '\' ]]; then
-  if [ -z "$MSWINHOME" ]; then
-    MSWINHOME="$HOMEDRIVE$HOMEPATH"
-  fi
-  if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-    MSWINHOME="${MSWINHOME//\\/\\\\}"
-    FILE="${FILE/#\~\\/$MSWINHOME\\}"
-    FILE=$(wslpath -u "$FILE")
-  elif [ -n "$MSWINHOME" ]; then
-    FILE="${FILE/#\~\\/$MSWINHOME\\}"
-  fi
-fi
-
 FILE="${FILE/#\~\//$HOME/}"
 if [ ! -r "$FILE" ]; then
   echo "File not found ${FILE}"
@@ -75,32 +56,14 @@ if [ -z "$CENTER" ]; then
   CENTER=0
 fi
 
-# Sometimes bat is installed as batcat.
-if command -v batcat > /dev/null; then
-  BATNAME="batcat"
-elif command -v bat > /dev/null; then
-  BATNAME="bat"
-fi
+BATNAME="bat"
 
-if [ -z "$FZF_PREVIEW_COMMAND" ] && [ "${BATNAME:+x}" ]; then
-  ${BATNAME} --style="${BAT_STYLE:-numbers}" --color=always --pager=never \
-      --highlight-line=$CENTER -- "$FILE"
-  exit $?
-fi
+LINE_RANGE_FROM=$(( $CENTER - $(tput lines) / 2 ))
+LINE_RANGE_FROM=$(( $LINE_RANGE_FROM < 0 ? 0 : $LINE_RANGE_FROM ))
+LINE_RANGE_TO=$(( $LINE_RANGE_FROM + $(tput lines)))
 
-FILE_LENGTH=${#FILE}
-MIME=$(file --dereference --mime -- "$FILE")
-if [[ "${MIME:FILE_LENGTH}" =~ binary ]]; then
-  echo "$MIME"
-  exit 0
-fi
+${BATNAME} --style="numbers" \
+    --line-range "$LINE_RANGE_FROM:$LINE_RANGE_TO" \
+    --color=always --pager=never \
+    --highlight-line=$CENTER -- "$FILE"
 
-DEFAULT_COMMAND="highlight -O ansi -l {} || coderay {} || rougify {} || cat {}"
-CMD=${FZF_PREVIEW_COMMAND:-$DEFAULT_COMMAND}
-CMD=${CMD//{\}/$(printf %q "$FILE")}
-
-eval "$CMD" 2> /dev/null | awk "{ \
-    if (NR == $CENTER) \
-        { gsub(/\x1b[[0-9;]*m/, \"&$REVERSE\"); printf(\"$REVERSE%s\n$RESET\", \$0); } \
-    else printf(\"$RESET%s\n\", \$0); \
-    }"
