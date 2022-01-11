fbea78f9380351ed52e1222fb8daba8a7a3c967a
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2022-01-11 18:48:40 +0100

    create a java keystore file on fix path with local root cas included

--- commons/nixos/certs.nix
+++ commons/nixos/certs.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs ? import <nixpkgs> {}, ... }:
 # Use this to test this file
 # echo -e ':p (import ./certs.nix {}).security.pki.certificateFiles' | nix repl
 with builtins;
@@ -13,8 +13,31 @@ let
           else []
       ) dir-content
     else [];
+  java-key-store = pkgs.stdenv.mkDerivation {
+    name = "java-key-store";
+
+    srcs = certificateFiles;
+    phases = "installPhase";
+    buildInputs = [ pkgs.jdk ];
+    installPhase = ''
+      echo "creating $out"
+      cp ${pkgs.jdk}/lib/openjdk/lib/security/cacerts $out
+      chmod +w $out
+      ls -al $out;
+      '' + (concatStringsSep "\n" (map (file: ''
+        keytool -importcert \
+          -noprompt \
+          -alias ${baseNameOf file} \
+          -keystore "$out" \
+          -storepass changeit \
+          -file ${file}
+      '') certificateFiles));
+  };
 in
  {
    security.pki.certificateFiles = certificateFiles;
+   environment.etc."java/cacerts" = {
+     source = java-key-store;
+   };
  }
 
