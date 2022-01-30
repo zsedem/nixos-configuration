500a056b858986d228ef006bc061ca87223cd57f
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2022-01-30 21:45:38 +0100

    set up Zen kernel

---
+++ commons/kernel.nix
@@ -0,0 +1,7 @@
+{ config, ... }:
+
+let
+  pkgs = (import <nixos-unstable> { config = config.nixpkgs.config; });
+in {
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linuxKernel.kernels.linux_zen;
+}
