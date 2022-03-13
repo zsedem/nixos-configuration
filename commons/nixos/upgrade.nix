bfae85c4c5ffaa2c967020423b5878a6584454ef
Adam Oliver Zsigmond <6388483+zsedem@users.noreply.github.com>
2022-03-13 22:28:33 +0100

    add some system notification for system update

--- commons/nixos/upgrade.nix
+++ commons/nixos/upgrade.nix
@@ -14,10 +14,19 @@
     path = [ pkgs.coreutils pkgs.gnutar pkgs.xz.bin pkgs.gzip pkgs.gitMinimal config.nix.package.out ];
     script = let
         nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
+        notify-send = "${pkgs.libnotify}/bin/notify-send";
+        sudo = "/run/wrappers/bin/sudo";
+        bash = "${pkgs.bash}/bin/bash";
       in
         ''
         nix-channel --update
-        ${nixos-rebuild} boot --upgrade --no-build-output
+        if ${nixos-rebuild} boot --upgrade --no-build-output; then
+          ${sudo} -u azsigmond \
+            ${bash} -c 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:1 ${notify-send} "Successful System Update"'
+        else
+          ${sudo} -u azsigmond \
+            ${bash} -c 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:1 ${notify-send} "FAILED System Update"'
+        fi
         '';
     startAt = "12:00";
   };
