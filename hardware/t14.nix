8a9316dbcc44c0b78d2f7082d98419303c9520f0
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2022-05-27 12:37:41 +0200

    reenable zen kernel

--- hardware/t14.nix
+++ hardware/t14.nix
@@ -1,13 +1,16 @@
 { config, lib, pkgs, mkDerivation, ... }:
 
-{
+let
+  latest-pkgs = (import <nixos-unstable> { config = config.nixpkgs.config; });
+in {
   imports = [
     <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
-    <nixos-hardware/lenovo/thinkpad/t14/amd/gen2>
+    <nixos-hardware/lenovo/thinkpad/t14/amd>
   ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelPackages = latest-pkgs.linuxPackages_zen;
   boot.extraModulePackages = [ ];
   hardware.opengl.enable = true;
   hardware.bluetooth.enable = true;
