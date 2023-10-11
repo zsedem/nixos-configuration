608f5417e62a035698dbbb718c26308b929716a8
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2023-10-11 11:50:02 +0200

    use latest vscode instead of stable

--- commons/languages.nix
+++ commons/languages.nix
@@ -60,7 +60,7 @@ in {
 
   config = with pkgs; with vscode-extensions; {
     environment.systemPackages =
-        plugins ++ [pkgs-stable.vscode] ++ onlyIf (flags.scala) [
+        plugins ++ [pkgs.vscode] ++ onlyIf (flags.scala) [
             (sbt.override { jre = choosen-jdk; })
             scalafmt
             scala-lang.scala
