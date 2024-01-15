97ba7f279af85b9cd6ba52fe7ae3f10fcf7ebec8
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2024-01-15 16:44:51 +0100

    fix zoom version

--- commons/zoom.nix
+++ commons/zoom.nix
@@ -1,9 +1,14 @@
 {config, lib, ...}:
 with lib;
 let
-  # I only follow the stable nixos release branch, because I want a stable OS, but
-  # when it comes to interactive tools, the newer the better.
-  pkgs = (import <nixos-unstable> { config = config.nixpkgs.config; });
+  # Set Zoom to release: 5.16.10.668
+  pkgs = (
+    import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/01962add7e97c051a6c2dd2a013d7e7c8ec2388a.tar.gz";
+    }) {
+	    config = config.nixpkgs.config;
+	}
+  );
   zoom-enabled = config.zsedem.zoom;
 in {
   options.zsedem.zoom = mkOption { type = types.bool; default = false; };
