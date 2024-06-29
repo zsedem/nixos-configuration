0b334cc2f479aeebaa465ec4f52ba2e6c8fd58e2
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2024-06-29 18:11:15 +0200

    update zoom to latest

--- commons/zoom.nix
+++ commons/zoom.nix
@@ -1,14 +1,14 @@
-{config, lib, ...}:
+{config, lib, pkgs, ...}:
 with lib;
 let
   # Set Zoom to release: 5.16.10.668
-  pkgs = (
-    import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/01962add7e97c051a6c2dd2a013d7e7c8ec2388a.tar.gz";
-    }) {
-	    config = config.nixpkgs.config;
-	}
-  );
+  #pkgs = (
+  #  import (builtins.fetchTarball {
+  #      url = "https://github.com/NixOS/nixpkgs/archive/01962add7e97c051a6c2dd2a013d7e7c8ec2388a.tar.gz";
+  #  }) {
+  #          config = config.nixpkgs.config;
+  #      }
+  #);
   zoom-enabled = config.zsedem.zoom;
 in {
   options.zsedem.zoom = mkOption { type = types.bool; default = false; };
