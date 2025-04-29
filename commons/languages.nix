ed625ff1ba958fcd5d620090a442d401524d2de6
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2025-04-29 11:08:01 +0200

    install bloop

--- commons/languages.nix
+++ commons/languages.nix
@@ -7,52 +7,60 @@ let
   flags = config.zsedem;
   choosen-jdk = config.programs.java.package;
   onlyIf = pred: l: if pred then l else [ ];
-in {
-  options.zsedem = let
-    flag = mkOption {
-      type = types.bool;
-      default = false;
+in
+{
+  options.zsedem =
+    let
+      flag = mkOption {
+        type = types.bool;
+        default = false;
+      };
+    in
+    {
+      scala = flag;
+      rust = flag;
+      k8s = flag;
     };
-  in {
-    scala = flag;
-    rust = flag;
-    k8s = flag;
-  };
 
   config = with pkgs; {
-    environment.systemPackages = [ pkgs.vscode ] ++ onlyIf (flags.scala) [
-      (sbt.override { jre = choosen-jdk; })
-      scalafmt
-      coursier
-      ammonite
-      jetbrains-toolbox
-
-    ] ++ onlyIf (flags.rust) [
-      rustup
-      gcc
-      llvmPackages.bintools-unwrapped
-      cmakeMinimal
-    ] ++ onlyIf (flags.k8s) [
-      kubectl
-      kubernetes-helm
-      kind
-      go
-      protobuf
-      argocd
-      argo-rollouts
-      awscli2
-    ] ++ [
-      # Feels pointless to create a python flag, since python is always used
-      # eventually for some scripting
-      python3Packages.pipx
-      python3Packages.yq
-      python3Packages.autoflake
-      python3Packages.cfn-lint
-      python3Packages.flake8
-      python3Packages.isort
-      nodejs
-      clang-tools
-      yajsv
-    ];
+    environment.systemPackages =
+      [ pkgs.vscode ]
+      ++ onlyIf (flags.scala) [
+        (sbt.override { jre = choosen-jdk; })
+        scalafmt
+        coursier
+        ammonite
+        jetbrains-toolbox
+        bloop
+      ]
+      ++ onlyIf (flags.rust) [
+        rustup
+        gcc
+        llvmPackages.bintools-unwrapped
+        cmakeMinimal
+      ]
+      ++ onlyIf (flags.k8s) [
+        kubectl
+        kubernetes-helm
+        kind
+        go
+        protobuf
+        argocd
+        argo-rollouts
+        awscli2
+      ]
+      ++ [
+        # Feels pointless to create a python flag, since python is always used
+        # eventually for some scripting
+        python3Packages.pipx
+        python3Packages.yq
+        python3Packages.autoflake
+        python3Packages.cfn-lint
+        python3Packages.flake8
+        python3Packages.isort
+        nodejs
+        clang-tools
+        yajsv
+      ];
   };
 }
