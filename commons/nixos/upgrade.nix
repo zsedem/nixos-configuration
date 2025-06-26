4ed228d6126c1fceb09ddbab99d56f7fada8cf89
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2025-06-26 12:18:25 +0200

    fix update to always pull latest repo version

--- commons/nixos/upgrade.nix
+++ commons/nixos/upgrade.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   systemd.services.nixos-upgrade = lib.mkIf (!config.zsedem.steam-enabled) {
@@ -6,20 +11,31 @@
     restartIfChanged = false;
     unitConfig.X-StopOnRemoval = false;
     serviceConfig.Type = "oneshot";
-    environment = config.nix.envVars //
-      { inherit (config.environment.sessionVariables) NIX_PATH;
+    environment =
+      config.nix.envVars
+      // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
         HOME = "/root";
-      } // config.networking.proxy.envVars ;
+      }
+      // config.networking.proxy.envVars;
 
-    path = [ pkgs.coreutils pkgs.gnutar pkgs.xz.bin pkgs.gzip pkgs.gitMinimal config.nix.package.out ];
-    script = let
+    path = [
+      pkgs.coreutils
+      pkgs.gnutar
+      pkgs.xz.bin
+      pkgs.gzip
+      pkgs.gitMinimal
+      config.nix.package.out
+    ];
+    script =
+      let
         nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
         notify-send = "${pkgs.libnotify}/bin/notify-send";
         sudo = "/run/wrappers/bin/sudo";
         bash = "${pkgs.bash}/bin/bash";
       in
-        ''
-        nix-channel --update
+      ''
+        nix-channel --update --option tarball-ttl 0
         if ${nixos-rebuild} boot --upgrade --no-build-output; then
           ${sudo} -u azsigmond \
             ${bash} -c 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:1 ${notify-send} "Successful System Update"'
@@ -27,7 +43,7 @@
           ${sudo} -u azsigmond \
             ${bash} -c 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:1 ${notify-send} "FAILED System Update"'
         fi
-        '';
+      '';
     startAt = "12:00";
 
     after = [ "network-online.target" ];
