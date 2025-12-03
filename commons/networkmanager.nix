3253f65fe895cbdc64053d21f7b2f8dda4704629
Zsigmond Adam Oliver <6388483+zsedem@users.noreply.github.com>
2025-12-03 19:46:48 +0100

    add missing openvpn

--- commons/networkmanager.nix
+++ commons/networkmanager.nix
@@ -1,6 +1,11 @@
-{  config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
   environment.systemPackages = [ pkgs.networkmanagerapplet ];
 }
